3b256c5e5210c04362368bd856b457ff15d3ee88
Mauricio <sir.silvabmauricio@gmail.com>
2023-08-03 20:51:08 -0400

    feat: aula1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      name: 'Mau',
+      counter: 0
+    };
+  }
+
+  handleClick = () => {
+    const { name } = this.state;
+    console.log(`Hello, ${name}!`);
+    this.setState({ name: 'Gandalf' });
+  }
+
+  handleIncrease = () => {
+    const { counter } = this.state;
+    this.setState({ counter: counter + 1});
+  }
+
+  render() {
+    const { name, counter } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            <span onClick={this.handleClick}>{name} - {counter}</span>
+            <br/>
+            <button onClick={this.handleIncrease}>+</button>
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
