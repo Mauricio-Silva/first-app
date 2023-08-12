bd462ab4bf1e4cd3185e38cac9c0e878c06ef211
Mauricio <sir.silvabmauricio@gmail.com>
2023-08-12 16:49:33 -0400

    feat: albums and photos

--- src/App.js
+++ src/App.js
@@ -1,76 +1,20 @@
 import './App.css';
 import { Component } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Cards from './components/Cards';
+import Albums from './components/Albums';
 
 class App extends Component {
-  timeoutUpdate = null
-  state = {
-    counter: 0,
-    posts: [
-      {
-        id: 1,
-        title: 'Title1',
-        body: 'Body1'
-      },
-      {
-        id: 2,
-        title: 'Title2',
-        body: 'Body2'
-      },
-      {
-        id: 3,
-        title: 'Title3',
-        body: 'Body3'
-      }
-    ]
-  };
-
-  componentDidMount() {
-    this.handleTimeOut()
-  }
-
-  componentDidUpdate() {
-    this.handleTimeOut()
-  }
-
-  componentWillUnmount() {
-    clearTimeout(this.timeoutUpdate)
-  }
-
-  handleTimeOut = () => {
-    const { posts, counter } = this.state;
-    this.timeoutUpdate = setTimeout(() => {
-      posts[0].title = 'other title'
-      this.setState({
-        posts,
-        counter: counter + 1
-      })
-    }, 2000)
-  }
-
   render() {
-    const { posts, counter } = this.state;
     return (
-      <div className="App">
-        <div>Counter: {counter}</div>
-        <table>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Title</th>
-              <th>Body</th>
-            </tr>
-          </thead>
-          <tbody>
-            {posts.map((post) => (
-              <tr key={post.id}>
-                <td>{post.id}</td>
-                <td>{post.title}</td>
-                <td>{post.body}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/cards" element={<Cards/>} />
+          <Route path="/albums" element={<Albums/>} />
+        </Routes>
+       </BrowserRouter>
     );
   }
 }
